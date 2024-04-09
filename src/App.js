a4d690d7564d6b3b7debc91ff4ab0fa071fc503f
Marco Arellanes <223728@ids.upchiapas.edu.mx>
2024-04-09 00:45:02 -0600

    cambios en el login y home listo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import Login from "./components/login";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/login';
+import Home from './home';
 
 function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginLayout />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function LoginLayout() {
   return (
     <div className="flex w-full h-screen">
       <div className="w-full flex items-center justify-center lg:w-1/2">
         <Login />
       </div>
-      <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-gray-200">
-        <div className="w-60 h-60 bg-gradient-to-tr from-orange-500 to-indigo-900 rounded-full animate-bounce">
-        </div>
-        <div className="w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg"></div>
+      <div className="hidden lg:flex h-full w-1/2 items-center justify-center bg-gray-200">
+        <div className="w-80 h-80 bg-gradient-to-tr from-orange-500 to-indigo-900 rounded-full animate-bounce"></div>
       </div>
     </div>
   );
